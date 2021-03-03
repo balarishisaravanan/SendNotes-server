a13bb5e5d31bb14155b5c662db05e360bf48baee
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-03-03 15:30:50 +0530

    added morgan middleware to log the requests in console

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const logger = require("./middleware/logger");
 //load env
 dotenv.config({ path: "./config/config.env" });
 // load routes
 const notes = require("./routes/notes");
+
 const app = express();
-//mount
+
+//mount routers
+app.use(logger);
 app.use("/api/v1/notes", notes);
 
 const PORT = process.env.PORT || 5000;
