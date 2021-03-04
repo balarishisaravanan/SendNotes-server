6186b53707645a55e978353fed343c11d952c54a
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-03-04 10:46:17 +0530

    establised connection to database

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //Load env
 dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
 // Load routes
 const notes = require("./routes/notes");
 
 const app = express();
+
 // Load Dev logger middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -16,9 +22,17 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/notes", notes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${5000}`
   )
 );
+
+//Handle Unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit the process
+  server.close(() => process.exit(1));
+});
