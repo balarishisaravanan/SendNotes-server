864379aecf008763168a5d65c95def8a19c7c86c
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-03-09 10:04:48 +0530

    added asyncHandler to follow DRY

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,85 +1,66 @@
 const ErrorResponse = require("../utils/ErrorResponse");
+const asyncHandler = require("../middleware/async");
 const { json } = require("express");
 const Notes = require("../models/Notes");
 
 // @desc Get all notes
 // @route GET /api/v1/notes/fetch
 // @access Public
-exports.getNotes = async (req, res, next) => {
-  try {
-    const notes = await Notes.find();
-    res.status(200).json({
-      success: true,
-      count: notes.length,
-      data: notes,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+exports.getNotes = asyncHandler(async (req, res, next) => {
+  const notes = await Notes.find();
+  res.status(200).json({
+    success: true,
+    count: notes.length,
+    data: notes,
+  });
+});
 
 // @desc Get single notes
 // @route GET /api/v1/notes/fetch/:id
 // @access Public
-exports.getSingleNotes = async (req, res, next) => {
-  try {
-    const singleNotes = await Notes.findById(req.params.id);
-    if (!singleNotes) {
-      return next(error);
-    }
-    res.status(200).json({ success: true, data: singleNotes });
-  } catch (error) {
-    next(error);
+exports.getSingleNotes = asyncHandler(async (req, res, next) => {
+  const singleNotes = await Notes.findById(req.params.id);
+  if (!singleNotes) {
+    return next(error);
   }
-};
+  res.status(200).json({ success: true, data: singleNotes });
+});
 
 // @desc Upload Notes
 // @route POST /api/v1/notes/upload/
 // @access Private
-exports.uploadNotes = async (req, res, next) => {
-  try {
-    const notes = await Notes.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: notes,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.uploadNotes = asyncHandler(async (req, res, next) => {
+  const notes = await Notes.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: notes,
+  });
+});
 
 // @desc Update  Notes
 // @route PUT /api/v1/notes/update/:id
 // @access Private
-exports.updateNotes = async (req, res, next) => {
-  try {
-    const notes = await Notes.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!notes) {
-      return next(error);
-    }
-    res.status(200).json({
-      success: true,
-      data: notes,
-    });
-  } catch (error) {
-    next(error);
+exports.updateNotes = asyncHandler(async (req, res, next) => {
+  const notes = await Notes.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!notes) {
+    return next(error);
   }
-};
+  res.status(200).json({
+    success: true,
+    data: notes,
+  });
+});
 
 // @desc Delete Notes
 // @route DELETE /api/v1/notes/delete/:id
 // @access Private
-exports.deleteNotes = async (req, res, next) => {
-  try {
-    const notes = await Notes.findByIdAndDelete(req.params.id);
-    if (!notes) {
-      next(error);
-    }
-    res.status(200).json({ success: true, data: {} });
-  } catch (error) {
+exports.deleteNotes = asyncHandler(async (req, res, next) => {
+  const notes = await Notes.findByIdAndDelete(req.params.id);
+  if (!notes) {
     next(error);
   }
-};
+  res.status(200).json({ success: true, data: {} });
+});
