bf51550816b8453e18f53dc3ae937052b38322cf
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-04-25 10:50:18 +0530

    added user registration and JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,10 +4,21 @@ const { json } = require("express");
 const User = require("../models/User");
 
 //@desc    Register User
-//@route   GET /api/v1/auth/register
+//@route   POST /api/v1/auth/register
 //@access  Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  //create token
+  const token = user.getSignedJwtToken();
+
   res.status(200).json({
     success: true,
+    token,
   });
 });
