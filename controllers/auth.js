16997ac480c0964b8cc17fe28bfe5b50ba5de3b6
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-04-25 12:24:48 +0530

    added user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,3 +22,41 @@ exports.register = asyncHandler(async (req, res, next) => {
     token,
   });
 });
+//@desc    Login User
+//@route   POST /api/v1/auth/login
+//@access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+
+  //validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide email and password", 400));
+  }
+
+  //Check for user existance
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    res.status(401).json({
+      success: false,
+      msg: "invalid credentials",
+    });
+  }
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    res.status(401).json({
+      success: false,
+      msg: "invalid credentials",
+    });
+  }
+
+  //create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token: token,
+  });
+});
