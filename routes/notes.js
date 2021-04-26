cc728c17609f4b7e2516bba13f34fc4e4831102d
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-04-26 11:53:58 +0530

    added protection to routes

--- routes/notes.js
+++ routes/notes.js
@@ -7,10 +7,13 @@ const {
   deleteNotes,
 } = require("../controllers/notes");
 const router = express.Router();
+
+const { protect } = require("../middleware/auth");
+
 router.route("/fetch").get(getNotes); //fetch notes
 router.route("/fetch/:id").get(getSingleNotes); //get single notes
-router.route("/upload").post(uploadNotes); //upload notes
-router.route("/update/:id").put(updateNotes); //update existing notes
-router.route("/delete/:id").delete(deleteNotes); //delete notes
+router.route("/upload").post(protect, uploadNotes); //upload notes
+router.route("/update/:id").put(protect, updateNotes); //update existing notes
+router.route("/delete/:id").delete(protect, deleteNotes); //delete notes
 
 module.exports = router;
