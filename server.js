158de84588f3c4141e5f45ff0b76b8773bc7ef11
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-04-26 09:39:22 +0530

    Added cookie-parser

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
 
@@ -19,6 +19,9 @@ const app = express();
 
 //Body Parser
 app.use(express.json());
+
+//Cookie Parser
+app.use(cookieParser());
 
 // Load Dev logger middleware
 if (process.env.NODE_ENV === "development") {
