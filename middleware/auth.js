d0dc0ea7814da5c1cbaa9d344af761181634e97e
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-04-26 11:52:36 +0530

    added jwt validation

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const ErrorResponse = require("../utils/ErrorResponse");
+const User = require("../models/User");
+
+//Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  //if token in cookies.
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+  //Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+  try {
+    //verify code
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+});
