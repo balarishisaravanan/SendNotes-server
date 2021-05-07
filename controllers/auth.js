feeaa0e0b2fe349b6839c22ad0d503a7bc3b5320
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-05-07 20:01:52 +0530

    Add logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,20 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user,
   });
 });
+//@desc  Logout
+//@route GET api/v1/auth/logout
+//@access Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 //@desc Forgot Password
 //@route POST api/v1/auth/forgotpassword
 //@access Public
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,14 @@ const {
   register,
   login,
   getMe,
+  logout,
   forgotPassword,
 } = require("../controllers/auth");
 const router = express.Router();
 const { protect } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 router.post("/me", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
 
