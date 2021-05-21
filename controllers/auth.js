9c9ceccb34090da548d46831dfc42c55cf666d01
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-05-21 16:49:39 +0530

    updated  logout mechanism

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,14 +61,11 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 //@desc  Logout
 //@route GET api/v1/auth/logout
 //@access Private
+
 exports.logout = asyncHandler(async (req, res, next) => {
-  res.cookie("token", "none", {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
-  res.status(200).json({
+  res.status(200).clearCookie("token").json({
     success: true,
-    data: {},
+    data: "logout success",
   });
 });
 
